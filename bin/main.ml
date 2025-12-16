bf6c570b4e30ce849dc97310ced8294289435d77
jacksons107 <jacksons@uchicago.edu>
2025-12-16 10:44:19 -0800

    solid action system

--- bin/main.ml
+++ bin/main.ml
@@ -1,134 +1,112 @@
 module IntMap = Map.Make(Int)
 
-type enemy = {hp : int; block : int; acts : action list}
+type game = {player : player; enemies : enemy IntMap.t}
 
-and player = {hp : int; block : int; mana : int; hand : hand; deck : deck}
+and enemy = {hp : int; block : int; actions : action list}
 
-and actor = Player of player | Enemy of enemy
-
-and action = 
-    | Attack of int
-    | Block of int
-    | Map of int (* increase the power number of every card in hand or deck *)
-
-and card = {id : int; cost : int; act : action; target : card_target_type; desc : string}
-
-(* Types of entities that can be targeted by cards. *)
-and card_target_type = SingleEnemy | AllEnemies | SingleCard | PlayerHand | PlayerDeck
-
-and hand = card list
+and player = {hp : int; block : int; hand : hand; deck : deck}
 
 and deck = card list
 
-(* Entities that can be the target of actions. *)
-type target = 
-    | Player of player
-    | Enemy of enemy
-    | Enemies of enemy IntMap.t
-    | Card of card
-    | Hand of hand
-    | Deck of deck
+and hand = card list
 
-type game = {player : player; enemies : enemy IntMap.t}
+and card = {id : int; cost : int; action_type : action_type}
 
+and action_type = 
+    | Attack of int * int
+    | Block of int
+    | PowInc of int
 
-let attack2 = {id = 0; cost = 1; act = Attack 2; target = SingleEnemy; desc = "Attack for 2 damage."}
-let block2 = {id = 1; cost = 1; act = Block 2; target = SingleEnemy; desc = "Add 2 block."}
+and action = target -> game -> game
 
-let increasePower power card = 
-    match card.act with
-        | Attack d -> {card with act = Attack (d + power)}
-        | Block b -> {card with act = Block (b + power)}
-        | Map p -> {card with act = Map (p + power)}
+and target = Player | Enemy of int | Card of int
 
-let instantiateAction (action : action) (actor : actor) : (target -> target) = 
-    match action with
-        | Attack d -> 
-            fun entity -> 
-                let h = entity.hp - d in {entity with hp = h}
-        | Block b ->
-            fun entity -> 
-                let blk = entity.block + b in {entity with block = blk}
-        | Map p ->
-            fun cards ->
-                List.map (increasePower p) cards
+(* General helpers to update an enemy stats. *)
+let update_enemy_hp new_hp enemy_opt = 
+    match enemy_opt with
+        | Some enemy -> Some {enemy with hp = new_hp}
+        | None -> None
 
-let print_enemy id (e : enemy) =
-    Printf.printf "Enemy ID: %d HP: %d\n" id e.hp
-
-let print_enemies (game : game) = 
-    IntMap.iter (fun x -> print_enemy x) game.enemies
-
-let read_int () =
-  try int_of_string (read_line ())
-  with _ ->
-    print_endline "Invalid number.";
-    read_int ()
-
-let selectEnemy enemies = 
-    print_string "Select an enemy to target: ";
-    IntMap.iter (fun x -> print_enemy x) enemies;
-    let selected_id = read_int () in IntMap.find selected_id enemies
-
-let print_hand (hand : hand) = 
-    List.iteri
-        (fun i c -> Printf.printf "  [%d] ID %d %s\n" i c.id c.desc)
-        hand
-
-let rec selectCard hand = 
-    print_string "Select an card to use: ";
-    print_hand hand;
-    let selected_idx = read_int () in 
-        if selected_idx < 0 || selected_idx >= List.length hand then (
-            print_endline "Invalid card index.";
-            selectCard hand
-        ) else
-            List.nth hand selected_idx
-
-let findCardTargets card game = 
-    match card.target with
-        | SingleEnemy -> Enemy (selectEnemy game.enemies)
-        | AllEnemies -> Enemies game.enemies
-        | SingleCard -> Card (selectCard game.player.hand)
-        | PlayerHand -> Hand game.player.hand
-        | PlayerDeck -> Deck game.player.deck
+let update_enemy_block new_block enemy_opt = 
+    match enemy_opt with
+        | Some enemy -> Some {enemy with block = new_block}
+        | None -> None
 
 
-(* 
-(* Choose the action the enemy will take. *)
-let chooseEnemyAction enemy = *)
+(* --- Attack action builders --- *)
+let attack damage target game = 
+    match target with
+        | Player -> 
+            let new_hp = game.player.hp - damage in
+            let p = game.player in
+            {game with player = {p with hp = new_hp}}
+        | Enemy id ->
+            let e = IntMap.find id game.enemies in
+            let new_hp = e.hp - damage in
+            let new_enemies = IntMap.update id (update_enemy_hp new_hp) game.enemies in
+            {game with enemies = new_enemies}
+        | Card _ ->
+            failwith "Cannot attack a card."
 
-let print_player (p : player) =
-    Printf.printf "Player HP: %d\n" p.hp;
-    Printf.printf "Hand:\n";
-    print_hand p.hand;
-    Printf.printf "Deck: %d\n" (List.length p.deck)
+let attack_multiple damage times = 
+    let attack_d = attack damage in 
+    let rec attack_times t target game = 
+        if t = 0 then game else attack_times (t-1) target (attack_d target game)
+    in
+    ((attack_times times) : action)
 
-let player_turn (game : game) =
-    print_endline "\n=== Player Turn ===";
-    print_player game.player;
-    print_enemies game;
+(* --- Block action builders --- *)
+let block block target game = 
+    match target with
+        | Player -> 
+            let new_block = game.player.block + block in
+            let p = game.player in
+            {game with player = {p with block = new_block}}
+        | Enemy id ->
+            let e = IntMap.find id game.enemies in
+            let new_block = e.block - block in
+            let new_enemies = IntMap.update id (update_enemy_block new_block) game.enemies in
+            {game with enemies = new_enemies}
+        | Card _ ->
+            failwith "Cannot add block to a card."
 
-    (* Player selects card from hand. *)
-    let selected_card = selectCard game.player.hand in
-    (* Player selects a target for the card. *)
-    let target = findCardTargets selected_card game in
-    (* Instantiate the action from the selected card. *)
-    let action = instantiateAction selected_card.act in
-    (* Apply the action to the target. *)
-    let new_target = action target in 
-    (* Update the game with the result of appying the action to the target. *)
-    let new_game = update_game game new_target
+(* --- Map action builders --- *)
+let incr_card_power power card = 
+    match card.action_type with
+        | Attack (d, t) -> {card with action_type = Attack (d+power, t)}
+        | Block b -> {card with action_type = Block (b+power)}
+        | PowInc p -> {card with action_type = PowInc (p+power)}
 
-let rec game_loop (game : game) =
-  if e.hp <= 0 then
-    print_endline "\nYou win!"
-  else if p.hp <= 0 then
-    print_endline "\nYou lose!"
-  else
-    let new_game = player_turn game in
-    game_loop new_game
+let replace_card idx (new_card : card) (hand : hand) = 
+    List.mapi (fun i current_element ->
+        if i = idx then new_card else current_element
+    ) hand
 
-let () =
-  print_endline "Welcome to Caml: The Ultimate!";
-  game_loop initial_game
+let incr_power power target game = 
+    match target with
+        | Card idx ->
+            let card = List.nth game.player.hand idx in
+            let new_card = incr_card_power power card in
+            let new_hand = replace_card idx new_card game.player.hand in
+            let new_player = {game.player with hand = new_hand} in
+            {game with player = new_player}
+        | _ -> failwith "Can only increase power of a card."
+
+(* Instantiate an action from an action_type. *)
+let instantiate_action action_type = 
+    match action_type with
+        | Attack (d, t) -> attack_multiple d t
+        | Block b -> block b
+        | PowInc p -> incr_power p
+
+
+(* ======= Example actions =========*)
+
+(* Attack for 2 damage 1 time. *)
+let attack_2 = instantiate_action (Attack (2, 1))
+(* Attack for 2 damage 4 times. *)
+let attack_2_4 = instantiate_action (Attack (2, 4))
+(* Add 3 block. *)
+let block_3 = instantiate_action (Block 3)
+(* Increase the power stat of a card by 1. *)
+let power_inc_1 = instantiate_action (PowInc 1)
